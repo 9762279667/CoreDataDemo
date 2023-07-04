88b233ab6f0110ab15e3cb2136f6691af3dbe014
Nitin Kalokhe <nitin.kalokhe@godigit.com>
2023-07-04 16:13:06 +0530

    some practice code

---
+++ CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Nitin Kalokhe on 19/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
